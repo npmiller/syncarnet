a213955e63349db01feb0e0317eeaedd94ef1d3a
nmiller <nicolas.miller@insa-rouen.fr>
2014-03-09 17:28:59 +0100

    Adds basic handling of deleted tasks

--- application/src/fr/insarouen/asi/notesync/tasks/TaskList.java
+++ application/src/fr/insarouen/asi/notesync/tasks/TaskList.java
@@ -1,11 +1,14 @@
 package fr.insarouen.asi.notesync.tasks;
 
+import java.util.UUID;
 import java.util.ArrayList;
 import java.util.Collections;
 
 import java.io.Serializable;
 
 public class TaskList extends ArrayList<Task> implements Serializable {
+	private ArrayList<UUID> deletedTasks = new ArrayList<UUID>();
+
 	/**
 	 * Inserts a new task at the adequate position based on its due date and priority.
 	 */
@@ -19,15 +22,48 @@ public class TaskList extends ArrayList<Task> implements Serializable {
 	}
 
 	/**
+	 * Returns whether or not a Task has been deleted from the TaskList
+	 */
+	public boolean deleted(Task task) {
+		return deletedTasks.contains(task);
+	}
+
+	/**
+	 * Removes a task from the TaskList and stores its UUID
+	 */
+	@Override
+	public boolean remove(Object o) {
+		deletedTasks.add(((Task)o).getUUID());
+		return super.remove(o);
+	}
+
+	/**
+	 * Removes a task from the TaskList and stores its UUID
+	 */
+	@Override
+	public Task remove(int position) {
+		Task task = super.remove(position);
+		deletedTasks.add(task.getUUID());
+		return task;
+	}
+
+	/**
 	 * Merges two TaskLists into one
 	 */
 	public static TaskList merge(TaskList tl1, TaskList tl2) {
 		TaskList tf = tl2;
 		Task t2;
-		for (Task t : tl1) {
+		for(Task t : tf) {
+			if(tl1.deleted(t)) {
+				tf.remove(t);
+			}
+		}
+		for(Task t : tl1) {
 			int pos = tf.indexOf(t);
 			if(pos == -1) {
-				tf.add(t);
+				if(!tf.deleted(t)) {
+					tf.add(t);
+				}
 			} else {
 				t2 = tf.get(pos);
 				if(t.getModified() != t2.getModified()) {
