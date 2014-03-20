1245a690165f2098dc420b4e1ef3525473dd3f6e
nmiller <nicolas.miller@insa-rouen.fr>
2014-03-20 16:58:01 +0100

    Add more extensive testing for TaskList

--- tests/src/fr/insarouen/asi/notesync/tests/tasks/TaskListTest.java
+++ tests/src/fr/insarouen/asi/notesync/tests/tasks/TaskListTest.java
@@ -12,10 +12,6 @@ public class TaskListTest {
 	private TaskList tl1;
 	private TaskList tl2;
 
-	@Before
-	public void setUp() {
-	}
-
 	@Test
 	public void test_order_priority() {
 		TaskList tl1 = new TaskList();
@@ -25,6 +21,62 @@ public class TaskListTest {
 		assertEquals(tl1.get(0).getDescription(), "t3");
 		assertEquals(tl1.get(1).getDescription(), "t1");
 		assertEquals(tl1.get(2).getDescription(), "t2");
+	}
+
+	@Test
+	public void test_order_calendar() {
+		TaskList tl1 = new TaskList();
+		Calendar c1 = Calendar.getInstance();
+		c1.set(2014,1,2);
+		Calendar c2 = Calendar.getInstance();
+		c2.set(2014,1,3);
+		Calendar c3 = Calendar.getInstance();
+		c3.set(2014,1,1);
+		tl1.add(new Task("t1", c1, null, Priority.MEDIUM)); 
+		tl1.add(new Task("t2", c2, null, Priority.LOW)); 
+		tl1.add(new Task("t3", c3, null, Priority.HIGH)); 
+		assertEquals(tl1.get(0).getDescription(), "t3");
+		assertEquals(tl1.get(1).getDescription(), "t1");
+		assertEquals(tl1.get(2).getDescription(), "t2");
+	}
+
+	@Test
+	public void test_order_mixed() {
+		TaskList tl1 = new TaskList();
+		Calendar c1 = Calendar.getInstance();
+		c1.set(2014,1,2);
+		Calendar c2 = Calendar.getInstance();
+		c2.set(2014,1,1);
+		tl1.add(new Task("t1", c1, null, Priority.MEDIUM)); 
+		tl1.add(new Task("t2", c2, null, Priority.LOW)); 
+		tl1.add(new Task("t3", c2, null, Priority.HIGH)); 
+		assertEquals(tl1.get(0).getDescription(), "t3");
+		assertEquals(tl1.get(1).getDescription(), "t2");
+		assertEquals(tl1.get(2).getDescription(), "t1");
+	}
+
+	@Test
+	public void test_remove() {
+		Task t = new Task("t1", null, null, Priority.MEDIUM);
+		TaskList tl1 = new TaskList();
+		tl1.add(t); 
+		tl1.add(new Task("t2", null, null, Priority.LOW)); 
+		tl1.add(new Task("t3", null, null, Priority.HIGH)); 
+		tl1.remove(t);
+		assertTrue(tl1.deleted(t));
+		assertFalse(tl1.contains(t));
+	}
+
+	@Test
+	public void test_remove_index() {
+		Task t = new Task("t1", null, null, Priority.HIGH);
+		TaskList tl1 = new TaskList();
+		tl1.add(t); 
+		tl1.add(new Task("t2", null, null, Priority.LOW)); 
+		tl1.add(new Task("t3", null, null, Priority.MEDIUM)); 
+		tl1.remove(0);
+		assertTrue(tl1.deleted(t));
+		assertFalse(tl1.contains(t));
 	}
 
 	@Test
@@ -39,9 +91,30 @@ public class TaskListTest {
 		tl2.add(new Task("t22", null, null, Priority.LOW)); 
 		tl2.add(new Task("t23", null, null, Priority.HIGH)); 
 		tl2.remove(t);
-		assertEquals(true, tl2.deleted(t));
+		assertTrue(tl2.deleted(t));
 		TaskList tf = TaskList.merge(tl1, tl2);
-		assertEquals(true, tf.deleted(t));
+		assertTrue(tf.deleted(t));
 		assertEquals(4, tf.size());
+	}
+
+	@Test
+	public void test_merge() {
+		Task t = new Task("t11", null, null, Priority.MEDIUM);
+		TaskList tl1 = new TaskList();
+		tl1.add(t); 
+		tl1.add(new Task("t12", null, null, Priority.LOW)); 
+		tl1.add(new Task("t13", null, null, Priority.HIGH)); 
+		TaskList tl2 = new TaskList();
+		tl2.add(t);
+		t.setDescription("t11-test");
+		tl2.add(new Task("t22", null, null, Priority.LOW)); 
+		tl2.add(new Task("t23", null, null, Priority.HIGH)); 
+		TaskList tf = TaskList.merge(tl1, tl2);
+		assertEquals("t23", tf.get(0).getDescription());
+		assertEquals("t13", tf.get(1).getDescription());
+		assertEquals("t11-test", tf.get(2).getDescription());
+		assertEquals("t22", tf.get(3).getDescription());
+		assertEquals("t12", tf.get(4).getDescription());
+		assertEquals(5, tf.size());
 	}
 }
