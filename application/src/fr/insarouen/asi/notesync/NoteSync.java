997c52467b28305d88862250f2ce791260271c67
nyddogghr <nyddogghr.fennrys@hotmail.fr>
2014-05-09 14:59:47 +0200

    cleaning

--- application/src/fr/insarouen/asi/notesync/NoteSync.java
+++ application/src/fr/insarouen/asi/notesync/NoteSync.java
@@ -62,15 +62,6 @@ public class NoteSync extends Activity implements TaskAddFragment.Callbacks,
 		   private BroadcastReceiver receiver = null;
 		   private ProgressDialog progressDialog = null;
 		   private PeerListDialog peerListDialog;
-		   // Message types sent from the BluetoothChatService Handler
-		   public static final int MESSAGE_STATE_CHANGE = 1;
-		   public static final int MESSAGE_READ = 2;
-		   public static final int MESSAGE_WRITE = 3;
-		   public static final int MESSAGE_DEVICE_NAME = 4;
-		   public static final int MESSAGE_TOAST = 5;
-		   // Key names received from the BluetoothChatService Handler
-		   public static final String DEVICE_NAME = "device_name";
-		   public static final String TOAST = "toast";
 		   // Intent request codes
 		   private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
 		   private static final int REQUEST_CONNECT_DEVICE_INSECURE = 2;
